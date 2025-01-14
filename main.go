6dbd5181530b018c15d4f8346509e81aef19212e
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-01-14 23:18:32 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Pong",
+		})
+	})
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, "helloword")
+	})
+	r.Run()
+}
